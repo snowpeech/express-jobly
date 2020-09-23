0fc9c3b2d0d41ae289f759e5b6e675c55dfbf49a
lydngai <ngai.lydia@gmail.com>
2020-09-23 13:55:41 -0500

    restructuring company routes into model methods

--- models/company.js
+++ models/company.js
@@ -1,1 +1,82 @@
-// write this later when ready to restructure
+const jsonschema = require("jsonschema");
+const companySchemaNew = require("../schemas/companySchemaNew.json");
+const companySchemaUpdate = require("../schemas/companySchemaUpdate.json");
+const sqlForPartialUpdate = require("../helpers/partialUpdate");
+const ExpressError = require("../helpers/expressError");
+
+const db = require("../db");
+
+class Company {
+  static async getAll(search, min_employees, max_employees) {
+    let counter = 1;
+    let values = [];
+
+    let getCompanies = `SELECT handle, name, num_employees, description, logo_url FROM companies`;
+
+    if (parseInt(max_employees) < parseInt(min_employees)) {
+      throw new ExpressError(
+        "min employees should not be greater than max employees",
+        400
+      );
+    }
+
+    if (search) {
+      getCompanies += ` WHERE (name LIKE $${counter} OR handle LIKE $${counter}) `;
+      values.push(`%${search}%`);
+      counter++;
+    }
+
+    if (min_employees) {
+      if (search) {
+        getCompanies += `AND num_employees >= $${counter}`;
+      } else {
+        getCompanies += ` WHERE num_employees >= $${counter}`;
+      }
+      values.push(parseInt(min_employees));
+      counter++;
+    }
+
+    if (max_employees) {
+      if (search || min_employees) {
+        getCompanies += ` AND num_employees <= $${counter}`;
+      } else {
+        getCompanies += ` WHERE num_employees <= $${counter}`;
+      }
+      values.push(parseInt(max_employees));
+    }
+
+    let results = await db.query(getCompanies, values);
+
+    return results.rows;
+  }
+
+  static async getOne(handle) {
+    const coRes = await db.query(
+      `SELECT handle, name, num_employees, description, logo_url FROM companies WHERE handle = $1`,
+      [handle]
+    );
+    if (coRes.rows.length === 0) {
+      throw new ExpressError(`No company with handle ${handle} was found`, 400);
+    }
+    const company = coRes.rows[0];
+    const jobRes = await db.query(
+      `SELECT  id, title, salary, equity, date_posted FROM jobs WHERE company_handle = $1`,
+      [handle]
+    );
+    company.jobs = jobRes.rows;
+    return company;
+  }
+
+  static async create(handle, name, num_employees, description, logo_url) {
+    const results = await db.query(
+      `INSERT INTO companies (handle, name, num_employees, description, logo_url) VALUES ($1, $2, $3, $4, $5) RETURNING handle, name, num_employees, description, logo_url`,
+      [handle, name, num_employees, description, logo_url]
+    );
+    return results.rows[0];
+  }
+
+  static async update(){}
+  // static async delete() => {}
+}
+
+module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -7,6 +7,8 @@ const companySchemaNew = require("../schemas/companySchemaNew.json");
 const companySchemaUpdate = require("../schemas/companySchemaUpdate.json");
 const sqlForPartialUpdate = require("../helpers/partialUpdate");
 
+const Company = require("../models/company");
+
 const db = require("../db");
 
 // const Book = require("../models/book");
@@ -14,47 +16,11 @@ const db = require("../db");
 router.get("/", async (req, res, next) => {
   // This should return the handle and name for all of the company objects. It should also allow for the following query string parameters
   try {
-    let counter = 1;
-    let values = [];
     const { search, min_employees, max_employees } = req.query;
 
-    let getCompanies = `SELECT handle, name, num_employees, description, logo_url FROM companies`;
+    let result = await Company.getAll(search, min_employees, max_employees);
 
-    if (parseInt(max_employees) < parseInt(min_employees)) {
-      throw new ExpressError(
-        "min employees should not be greater than max employees",
-        400
-      );
-    }
-
-    if (search) {
-      getCompanies += ` WHERE (name LIKE $${counter} OR handle LIKE $${counter}) `;
-      values.push(`%${search}%`);
-      counter++;
-    }
-
-    if (min_employees) {
-      if (search) {
-        getCompanies += `AND num_employees >= $${counter}`;
-      } else {
-        getCompanies += ` WHERE num_employees >= $${counter}`;
-      }
-      values.push(parseInt(min_employees));
-      counter++;
-    }
-
-    if (max_employees) {
-      if (search || min_employees) {
-        getCompanies += ` AND num_employees <= $${counter}`;
-      } else {
-        getCompanies += ` WHERE num_employees <= $${counter}`;
-      }
-      values.push(parseInt(max_employees));
-    }
-
-    let results = await db.query(getCompanies, values);
-
-    return res.json({ companies: results.rows });
+    return res.json({ companies: result });
   } catch (e) {
     return next(e);
   }
@@ -70,11 +36,15 @@ router.post("/", async (req, res, next) => {
       return next(error);
     }
     const { handle, name, num_employees, description, logo_url } = req.body;
-    const results = await db.query(
-      `INSERT INTO companies (handle, name, num_employees, description, logo_url) VALUES ($1, $2, $3, $4, $5) RETURNING handle, name, num_employees, description, logo_url`,
-      [handle, name, num_employees, description, logo_url]
+    const result = await Company.create(
+      handle,
+      name,
+      num_employees,
+      description,
+      logo_url
     );
-    return res.json({ company: results.rows[0] });
+
+    return res.json({ company: result });
   } catch (e) {
     return next(e);
   }
@@ -84,20 +54,9 @@ router.get("/:handle", async (req, res, next) => {
   //return a single company by its id
   try {
     const { handle } = req.params;
-    const coRes = await db.query(
-      `SELECT handle, name, num_employees, description, logo_url FROM companies WHERE handle = $1`,
-      [handle]
-    );
-    if (coRes.rows.length === 0) {
-      throw new ExpressError(`No company with handle ${handle} was found`, 400);
-    }
-    const company = coRes.rows[0];
-    const jobRes = await db.query(
-      `SELECT  id, title, salary, equity, date_posted FROM jobs WHERE company_handle = $1`,
-      [req.params.handle]
-    );
-    company.jobs = jobRes.rows;
-    return res.json({ company });
+    let result = await Company.getOne(handle);
+
+    return res.json({ company: result });
   } catch (e) {
     return next(e);
   }
@@ -106,7 +65,6 @@ router.get("/:handle", async (req, res, next) => {
 router.patch("/:handle", async (req, res, next) => {
   //update an existing company and return updated company
   // for every value passed in, create an array.
-  //ADD check to not update handle
   try {
     const validationResult = jsonschema.validate(req.body, companySchemaUpdate);
     if (!validationResult.valid) {
